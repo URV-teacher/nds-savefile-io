dd4e8c6488c27f9ae12281fc59b0f3290dc010ea
AleixMT <aleixaretra@gmail.com>
2025-01-14 03:22:19 +0100

    Initial commit

---
+++ source/hello-world.c
@@ -0,0 +1,18 @@
+﻿#include <nds.h>
+#include <stdio.h>
+#include "hello-world.h"
+
+int main(void)
+{
+	consoleDemoInit();
+	printf("Hello World\n");
+
+	do
+	{
+		swiWaitForVBlank();
+		
+	} while (1);
+	
+	return(0);
+}
+
